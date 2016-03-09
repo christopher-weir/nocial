0c056e0b51f5c813c23d02140baeefbcdfe8689f
Christopher Weir <christopher_weir@me.com>
2016-03-09 13:29:38 +0000

    added index and social constructor

---
+++ src/nocial.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * innie version number as a string.
+ */
+exports.version = '0.0.1';
+
+
+/**
+ * Nocial Options Object.
+ * This object can be passed to many of the API-level Innie methods to control
+ * various aspects of the engine. All keys are optional.
+ */
+var defaultOptions = {
+};
+var defaultInstance = null;
+
+
+exports.Nocial = function( _opts ) {
+};
+
+
+
+defaultInstance         = new exports.Nocial();
