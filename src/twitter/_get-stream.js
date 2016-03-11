a6b5f50afbec8d55d5c42ebd4a9f23d26f8b040a
Christopher Weir <christopher_weir@me.com>
2016-03-11 09:59:22 +0000

    updated stream

--- src/twitter/_get-stream.js
+++ src/twitter/_get-stream.js
@@ -1,16 +1,18 @@
 'use strict';
 
 var nocial = require('../nocial.js');
+var querystring = require('querystring');
 
-
-
-module.exports = function(type, params, accessToken, accessTokenSecret) {
+module.exports = function(type, params, accessToken, accessTokenSecret, dataCallback) {
 
     return new Promise(function(_resolve, _reject) {
         type = type.toLowerCase();
 
         var url = '';
+        var req = '';
         var method = 'GET';
+        var errorMessage = '';
+        var msg = [];
 
         switch (type) {
             case 'userstream':
@@ -26,19 +28,60 @@ module.exports = function(type, params, accessToken, accessTokenSecret) {
                 break;
             case 'firehose':
                 url = 'https://stream.twitter.com/1.1/statuses/firehose.json';
-            break;
+                break;
             case 'filter':
                 method = 'POST';
                 url = 'https://stream.twitter.com/1.1/statuses/filter.json';
                 break;
             default:
-                var errorMessage = 'Please specify an existing type.';
+                errorMessage = 'Please specify an existing type.';
                 _reject({
                     message: errorMessage,
-                    e: new Error(errorMessage)
+                    error: new Error(errorMessage)
                 });
                 return false;
         }
+
+
+        if (method === 'GET') {
+            req = this.oa.get(url + '?' + querystring.stringify(params), accessToken, accessTokenSecret);
+        } else {
+            req = this.oa.post(url, accessToken, accessTokenSecret, params, null);
+        }
+
+        req.addListener('response', function(res) {
+            res.setEncoding('utf-8');
+            res.addListener('data', function(chunk) {
+                if (chunk === '\r\n') {
+                    dataCallback(null, {}, chunk, res);
+                    return;
+                } else if (chunk.substr(chunk.length - 2) === '\r\n') {
+                    msg.push(chunk.substr(0, chunk.length - 2));
+                    var ret = msg.join('');
+                    msg = [];
+
+                    var parsedRet;
+                    try {
+                        parsedRet = JSON.parse(ret);
+                    } catch (e) {
+                        _reject({
+                            message: 'Error while parsing Twitter-Response.',
+                            error: e
+                        });
+                        return;
+                    }
+                    dataCallback(null, parsedRet, ret, res);
+                    return;
+                } else {
+                    msg.push(chunk);
+                    return;
+                }
+            });
+            res.addListener('end', function() {
+                _resolve(res);
+            });
+        });
+        req.end();
 
     });
 
