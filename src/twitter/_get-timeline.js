5cc861f8b79e7dd35229357726c06274881633af
Christopher Weir <christopher_weir@me.com>
2016-03-11 09:53:14 +0000

    Consistant error messages

--- src/twitter/_get-timeline.js
+++ src/twitter/_get-timeline.js
@@ -3,12 +3,13 @@
 var nocial = require('../nocial.js');
 var querystring = require('querystring');
 
-module.exports = function( type, params, accessToken, accessTokenSecret ) {
+module.exports = function(type, params, accessToken, accessTokenSecret) {
 
-    return new Promise(function( _resolve, _reject ) {
+    return new Promise(function(_resolve, _reject) {
         type = type.toLowerCase();
 
         var url = null;
+        var errorMessage = '';
 
         switch (type) {
             case 'home_timeline':
@@ -22,7 +23,12 @@ module.exports = function( type, params, accessToken, accessTokenSecret ) {
             case 'user_timeline':
             case 'user':
                 if (!params.user_id && !params.screen_name) {
-                    _reject('Always specify either an user_id or screen_name when requesting a user timeline.');
+
+                    errorMessage = 'Always specify either an user_id or screen_name when requesting a user timeline.';
+                    _reject({
+                        message: errorMessage,
+                        error: new Error(errorMessage)
+                    });
                     return false;
                 }
                 url = 'user_timeline';
@@ -32,22 +38,38 @@ module.exports = function( type, params, accessToken, accessTokenSecret ) {
                 url = 'retweets_of_me';
                 break;
             default:
-                _reject('Please specify an existing type.');
+
+                errorMessage = 'Please specify an existing type.';
+                _reject({
+                    message: errorMessage,
+                    error: new Error(errorMessage)
+                });
                 return false;
         }
 
         nocial.twitterOAuth.get(
-            nocial.options.twitter.baseUrl + 'statuses/' + url + '.json?' + querystring.stringify( params ),
+            nocial.options.twitter.baseUrl + 'statuses/' + url + '.json?' + querystring.stringify(params),
             accessToken,
             accessTokenSecret,
             function(error, data, response) {
                 if (error) {
-                    _reject(error);
+                    errorMessage = 'There was an error';
+                    _reject({
+                        message: errorMessage,
+                        error: new Error(error)
+                    });
                 } else {
                     try {
-                        _resolve(JSON.parse(data));
+                        _resolve({
+                            data: JSON.parse(data),
+                            response: response
+                        });
                     } catch (e) {
-                        _reject(e);
+                        errorMessage = 'There was an error parsing the data';
+                        _reject({
+                            message: errorMessage,
+                            error: new Error(e)
+                        });
                     }
                 }
             }
